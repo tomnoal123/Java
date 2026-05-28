517ebb00291c0ac7eb94bd0ef0e5460ba8986354
tomnoal123 <tomnoal123@gmail.com>
2026-05-28 14:35:45 +0200

    Update DaneOsobowe.java

--- src/DaneOsobowe.java
+++ src/DaneOsobowe.java
@@ -1,5 +1,5 @@
 public class DaneOsobowe {
     public static void main(String[] args) {
-        System.out.println("Tomasz Nosal \n 3 Maja \n 20a \n 500770490");
+        System.out.println("Hubert Nosal \n 3 Zawada \n 10 \n 500770490");
     }
 }
